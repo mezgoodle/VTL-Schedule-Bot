f1cdb08b28ae084011703720738dee3499bfe965
mezgoodle <mezgoodle@gmail.com>
2019-11-01 17:36:25 +0200

    Improving some functions
    Now we can easy detect which group is using.
    In help section I added link to Telegraph, where are groups` codes.
    In addition I have written function which battles against dums

--- bot/main.py
+++ bot/main.py
@@ -6,6 +6,27 @@ conn = sqlite3.connect(database)
 print(conn)
 bot = telebot.TeleBot(TG_TOKEN)
 
+# Group dictionary
+GROUP_DICT = {
+    '/group_pm1': 'pm1',
+    '/group_pm2': 'pm2',
+    '/group_pm3': 'pm3',
+    '/group_pm4': 'pm4',
+    '/group_lin3': 'lin3',
+    '/group_lin4': 'lin4',
+    '/group_itn1': 'itn1',
+    '/group_itn2': 'itn2',
+    '/group_itn3': 'itn3',
+    '/group_itn4': 'itn4',
+    '/group_pn1': 'pn1',
+    '/group_pn2': 'pn2',
+    '/group_ite3': 'ite3',
+    '/group_itr4': 'itr4',
+}
+
+# Group identification
+GROUP_ID = ''
+
 @bot.message_handler(commands=['start'])
 def handle_start(message):
     user_markup = telebot.types.ReplyKeyboardMarkup(True, False)
@@ -16,6 +37,10 @@ def handle_start(message):
 def handle_stop(message):
     hide_markup = telebot.types.ReplyKeyboardRemove()
     bot.send_message(message.from_user.id, 'Клавіатуру вимкнено', reply_markup=hide_markup)
+
+@bot.message_handler(commands=['group'])
+def handle_group_damn(message):
+    bot.send_message(message.from_user.id, 'Для почтаку встанови код своєї групи(/group_[назва групи]), наприклад, /group_pm1.')
 
 @bot.message_handler(commands=['help'])
 def handle_text(message):
@@ -29,6 +54,7 @@ def handle_text(message):
 /who - Підказує ім'я вчителя
 /group - Встановити групу
 /stop - Вимкнути клавіатуру
+https://telegra.ph/Kodi-grup-dlya-vtl-schedule-bot-10-25 - Коди груп
     """)
 
 @bot.message_handler(commands=['timetable'])
@@ -43,6 +69,16 @@ def handle_timetable(message):
 7. 14.25 - 15.10
 8. 15.15 - 16.00
     """)
+
+@bot.message_handler(content_types=['text'])
+def handle_group(message):
+    #bot.send_message(message.chat.id, str(type(message.text))) DEBUG
+    if str(message.text)[-1].isdigit():
+        bot.send_message(message.chat.id, GROUP_DICT[message.text])
+        print(GROUP_DICT[message.text])
+    else:
+        print('FALSE')
+        bot.send_message(message.chat.id, 'Надіюсь, ти правильно написав команду\U0001f600. Якщо не знаєш, що писати, переглянь /help.')
 
 # @bot.message_handler(content_types=["text"])
 # def handle_text(message):
